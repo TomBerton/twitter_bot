36592cadf80fc288304b74aa99f753865121881e
Tom Berton <trberton@gmail.com>
2017-11-27 11:24:31 -0600

    Update ChatterBot.py

--- ChatterBot.py
+++ ChatterBot.py
@@ -19,10 +19,10 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Create a function that tweets
 # Create a function that tweets
-def TweetOut(tweet_number):
-    api.update_status(
-        "Can't stop. Won't stop. Chatting! This is Tweet #%s!" %
-        tweet_number)
+#def TweetOut(tweet_number):
+  #  api.update_status(
+ #       "Can't stop. Won't stop. Chatting! This is Tweet #%s!" %
+  #      tweet_number)
 
 
 # Create a function that calls the TweetOut function every minute
